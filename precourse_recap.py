a009d86c2b3e34e39c5567dc9ae3ca7dfaeb468f
Becky <bjentwistle@gmail.com>
2023-04-03 17:09:01 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,19 @@
+# print(type("Hello"))
+# print(type(24))
+
+# my_fav_cheese = "vegan"
+# age = 47
+# print(age, my_fav_cheese)
+
+# number_1 = 22
+# number_2 = 13
+# sum_of_numbers = number_2 + number_1
+# print("Answer is ", sum_of_numbers)
+
+word_1 = "dinosaur"
+word_2 = "Ladybird"
+print(word_1.upper() + " " + word_2)
+print(word_1)
+
+word_1 = word_1.upper()
+print(word_1)
